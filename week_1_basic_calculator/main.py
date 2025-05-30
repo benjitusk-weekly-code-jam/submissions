07d64e3856e92e207729657d4c3d7c9d48ded575
benjitusk <benjitusk1@gmail.com>
2025-05-30 14:54:04 +0300

    update main.py template for week 1

--- week_1_basic_calculator/main.py
+++ week_1_basic_calculator/main.py
@@ -3,8 +3,15 @@
 
 
 def main():
-    """A standard hello world program."""
-    print("Hello, World!")
+    """A reminder to set up the proper folder structure."""
+    print("*******")
+    print("** Attention!")
+    print("** Please create a folder with your github username,")
+    print("** and move this file inside that folder.")
+    print("**")
+    print("** This will be the entry point for your calculator project.")
+    print("** After you move this file, please delete this message.")
+    print("*******")
 
 
 if __name__ == "__main__":
